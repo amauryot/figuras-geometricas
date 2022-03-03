78d5014acd77fcb482471740dc3032bb508cfcba
Amaury Tavares <amauryot@gmail.com>
2022-03-03 11:54:50 -0300

    v0.0.1 - Versão inicial do app.
    - Implementada apenas a GUI inicial do menu do Losango.

---
+++ FigurasGeometricas/src/main/MainFigurasGeometricas.java
@@ -0,0 +1,12 @@
+package main;
+
+import controller.Controller;
+
+public class MainFigurasGeometricas {
+
+	public static void main(String[] args) {
+		
+		Controller controller = new Controller();
+		controller.run();
+	}
+}
